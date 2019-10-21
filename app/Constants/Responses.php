e84a5ce4014f7c6bb2487bbc1c76268f3421fed4
Fanan Dala <fanan.dala@yahoo.com>
2019-10-21 17:30:21 +0100

    feat: added constants

---
+++ app/Constants/Responses.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Constants;
+
+class Responses
+{
+	const SERVER_ERROR = "Sorry, something went wrong. Please try again.";
+	const INPUT_ERROR = "Your input details are not correct. Please enter correct details and try again.";
+	const NEWSLETTER_SUCCESS = "You have been successfully added to our newsletter.";
+}
