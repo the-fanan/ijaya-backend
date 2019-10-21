28afc3b9d45701101c39693206f0630eb2c952fc
Fanan Dala <fanan.dala@yahoo.com>
2019-10-21 17:30:02 +0100

    test: udated test suite for contact

--- database/migrations/2019_10_21_145139_create_newsletter_subscribers_table.php
+++ database/migrations/2019_10_21_145139_create_newsletter_subscribers_table.php
@@ -16,7 +16,7 @@ class CreateNewsletterSubscribersTable extends Migration
         Schema::create('newsletter_subscribers', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
--- tests/Unit/ContactTest.php
+++ tests/Unit/ContactTest.php
@@ -9,10 +9,15 @@ use Illuminate\Http\Request;
 use Faker\Factory as Faker;
 use Tests\TestCase;
 use App\Http\Controllers\API\ContactController;
+use App\Constants\Responses as ResponseMessage;
 
 class ContactTest extends TestCase
 {
+    use DatabaseTransactions;
     private $contactController;
+    private $validSubscriber = ["name" => "Fanan Dala", "email" => "fanan123@yahoo.com"];
+    private $invalidSubscriber = ["email" => "fanan123"];
+
     public function setUp() :void
     {
         $this->contactController = new ContactController();
@@ -27,17 +32,40 @@ class ContactTest extends TestCase
      */
     public function testAddToNewsLetterSuccessful()
     {
-        $this->assertFalse(True);
+        $request = Request::create('/api/contact/subscribe', 'POST', $this->validSubscriber);
+        $response = $this->contactController->addToNewsletter($request);
+        $this->assertEquals(ResponseMessage::NEWSLETTER_SUCCESS, $response->getData()->message);
+        $this->assertDatabaseHas('newsletter_subscribers', $this->validSubscriber);
     }
 
     /**
-     * Tests whether addToNewsletter fails in if details are not complete
+     * Tests whether addToNewsletter fails if details are not complete
      *
      * @return void
      */
     public function testAddToNewsLetterFailForIncompleteDetails()
     {
-        $this->assertFalse(True);
+        $request = Request::create('/api/contact/subscribe', 'POST', $this->invalidSubscriber);
+        $response = $this->contactController->addToNewsletter($request);
+        $this->assertEquals(ResponseMessage::INPUT_ERROR, $response->getData()->message);
+        $this->assertObjectHasAttribute('name',$response->getData()->data);
+        $this->assertObjectHasAttribute('email',$response->getData()->data);
+        $this->assertDatabaseMissing('newsletter_subscribers', $this->invalidSubscriber);
+    }
+
+    /**
+     * Tests whether addToNewsletter fails for duplicate email
+     *
+     * @return void
+     */
+    public function testAddToNewsLetterFailForDuplicateEmail()
+    {
+        $request = Request::create('/api/contact/subscribe', 'POST', $this->validSubscriber);
+        $response = $this->contactController->addToNewsletter($request);
+        $response2 = $this->contactController->addToNewsletter($request);
+
+        $this->assertEquals(ResponseMessage::INPUT_ERROR, $response2->getData()->message);
+        $this->assertObjectHasAttribute('email',$response2->getData()->data);
     }
 
     public function tearDown() :void
