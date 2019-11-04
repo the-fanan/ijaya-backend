f69c2f622df886cb9d48fb1806933f80126ae2dd
Fanan Dala <fanan.dala@yahoo.com>
2019-11-04 14:58:36 +0100

    feat(cors): added website domain name

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['http://localhost:3000'],
+    'allowedOrigins' => ['http://localhost:3000', 'https://ijayamanagement.com'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
